048050603f48e843d671a66ee16ee7be96f5f44c
Dragon-L <530593201@qq.com>
2018-03-07 12:11:03 +0800

    implement with tensorflow

---
+++ main_tensorflow.py
@@ -0,0 +1,28 @@
+from utils.tool import load_data, calculate_precision_rate, save_to_csv, save_dict_to_pkl, load_dict_from_pkl
+from train_with_tensorflow.deep_learning import training_parameters
+
+USELESS_ROWS_FOR_TRAIN = [0, 1, 3, 8, 10]
+USELESS_ROWS_FOR_TEST = [0, 2, 7, 9]
+SURVIVED_ID = 1
+LAYER_DIM = [7, 5, 5, 3, 3, 1]
+activation = 'relu'
+TRAIN_FILE = './data/train.csv'
+TEST_FILE = './data/test.csv'
+PARAMETERS_FILE = './data/parameters.pkl'
+SUBMISSION_FILE = './data/submission.csv'
+
+X_train, Y_train = load_data(TRAIN_FILE, USELESS_ROWS_FOR_TRAIN, SURVIVED_ID)
+# parameters = initialize_parameters(X_train, LAYER_DIM)
+# parameters = load_dict_from_pkl(PARAMETERS_FILE)
+
+parameters = training_parameters(X_train, Y_train, LAYER_DIM, activation, '', 0.01)
+
+# save_dict_to_pkl(parameters, PARAMETERS_FILE)
+#
+# Y_predict = predict(X_train, parameters, len(LAYER_DIM) - 1, activation)
+# precision_rate = calculate_precision_rate(Y_predict, Y_train)
+# print('precision rate is: %f' %precision_rate)
+#
+# X_test, _ = load_data(TEST_FILE, USELESS_ROWS_FOR_TEST)
+# Y_test_predict = predict(X_test, parameters, len(LAYER_DIM) - 1, activation)
+# save_to_csv(Y_test_predict, SUBMISSION_FILE)
---
+++ train_with_tensorflow/deep_learning.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+from mannual.deep_learning import initialize_parameters
+
+
+def initialize_parameters(layer_dim):
+    parameters = {}
+    for l in range(1, len(layer_dim)):
+        # parameters['W' + str(l)] = tf.placeholder(tf.float32, shape=[layer_dim[l], layer_dim[l-1]])
+        # parameters['b' + str(l)] = tf.placeholder(tf.float32, shape=[layer_dim[l], 1])
+        parameters['W' + str(l)] = tf.get_variable('W' + str(l), [layer_dim[l], layer_dim[l - 1]],
+                                                   initializer=tf.contrib.layers.xavier_initializer())
+        parameters['b' + str(l)] = tf.get_variable('b' + str(l), [layer_dim[l], 1], initializer=tf.zeros_initializer())
+    return parameters
+
+
+def forward_propagation(X, parameters, layer_len, activation):
+    cache = {}
+    cache['A0'] = X
+    for l in range(1, layer_len):
+        cache['Z' + str(l)] = tf.add(tf.matmul(parameters['W' + str(l)], cache['A' + str(l-1)]), parameters['b' + str(l)])
+        if activation == 'relu':
+            cache['A' + str(l)] = tf.nn.relu(cache['Z' + str(l)])
+
+    return cache['Z' + str(layer_len - 1)]
+
+
+def compute_cost(Y_train, Zl):
+    logits = tf.transpose(Zl)
+    labels = tf.transpose(Y_train)
+    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels))
+    return cost
+
+def training_parameters(X_train, Y_train, layer_dim, activation, parameters_origin, learning_rate, num_iterations = 1000):
+    X = tf.placeholder(tf.float32, shape=[layer_dim[0], None])
+    Y = tf.placeholder(tf.float32, shape=[1, None])
+    parameters = initialize_parameters(layer_dim)
+
+    Zl = forward_propagation(X, parameters, len(layer_dim), activation)
+    cost = compute_cost(Y, Zl)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(num_iterations):
+            _, current_cost = sess.run([optimizer, cost], feed_dict={X: X_train, Y: Y_train})
+
+            if i % 1000 == 0:
+                print("Cost after iterations %i: %f" % (i, current_cost))
+
+        parameters = sess.run(parameters)
+
+        correct_prediction = tf.equal(tf.argmax(Zl), tf.argmax(Y))
+
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+
+        print('Train Accuracy:', accuracy.eval({X: X_train, Y: Y_train}))
+
+    return parameters
