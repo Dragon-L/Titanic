4fef4fe8e31b385b1106d7bf08c2b8d16e9c8523
Dragon-L <530593201@qq.com>
2018-03-14 17:59:05 +0800

    calculate prediction

--- train_with_tensorflow/deep_learning.py
+++ train_with_tensorflow/deep_learning.py
@@ -5,8 +5,6 @@ from mannual.deep_learning import initialize_parameters
 def initialize_parameters(layer_dim):
     parameters = {}
     for l in range(1, len(layer_dim)):
-        # parameters['W' + str(l)] = tf.placeholder(tf.float32, shape=[layer_dim[l], layer_dim[l-1]])
-        # parameters['b' + str(l)] = tf.placeholder(tf.float32, shape=[layer_dim[l], 1])
         parameters['W' + str(l)] = tf.get_variable('W' + str(l), [layer_dim[l], layer_dim[l - 1]],
                                                    initializer=tf.contrib.layers.xavier_initializer())
         parameters['b' + str(l)] = tf.get_variable('b' + str(l), [layer_dim[l], 1], initializer=tf.zeros_initializer())
@@ -50,8 +48,13 @@ def training_parameters(X_train, Y_train, layer_dim, activation, parameters_orig
 
         parameters = sess.run(parameters)
 
-        correct_prediction = tf.equal(tf.argmax(Zl), tf.argmax(Y))
+        half_value = tf.constant(0.5, shape=[Y_train.shape[0], Y_train.shape[1]])
+        true_value = tf.constant(1.0, shape=[Y_train.shape[0], Y_train.shape[1]])
+        result = tf.nn.sigmoid(Zl)
+        Y_predict = tf.greater_equal(result, half_value)
+        Y_target = tf.equal(Y, true_value)
 
+        correct_prediction = tf.equal(Y_predict, Y_target)
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
 
         print('Train Accuracy:', accuracy.eval({X: X_train, Y: Y_train}))
